5da26cd8169f4e05c2cae1674b167dd1f23c2c73
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-03-22 22:39:36 -0400

    Add code to solve polynomial by user input

--- c-modern-approach/chapter-2/projects/project-5.c
+++ c-modern-approach/chapter-2/projects/project-5.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
+    int x;
+
+    printf("Enter a value for x: ");
+    scanf_s("%d", &x);
+
+    int polynomial = 3 * (x * x * x * x * x) + 2 * (x * x * x * x) -
+                     5 * (x * x * x) - (x * x) + 7 * x - 6;
+
+    printf("Solved for x: %d", polynomial);
     return 0;
 }
