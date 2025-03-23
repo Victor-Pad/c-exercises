233b0c63b5ea238cba9699aa8b2dad61c5106d86
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-03-22 22:19:07 -0400

    Add code to calculate the volume of a sphere

--- c-modern-approach/chapter-2/projects/project-2.c
+++ c-modern-approach/chapter-2/projects/project-2.c
@@ -1,1 +1,9 @@
 #include <stdio.h>
+#define RADIUS 10
+#define FOUR_THREE_FRACTION (4.0f / 3.0f)
+#define PI 3.14
+
+int main(void) {
+    float volume = FOUR_THREE_FRACTION * PI * (RADIUS * RADIUS * RADIUS);
+    printf("Volume of sphere: %f", volume);
+}
