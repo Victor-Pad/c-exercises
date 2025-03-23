5a49702da79a9702661dac423b2242bad18c05db
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-03-22 22:44:55 -0400

    Add code to apply horner's rule to the polynomial

--- c-modern-approach/chapter-2/projects/project-6.c
+++ c-modern-approach/chapter-2/projects/project-6.c
@@ -1,1 +1,12 @@
 #include <stdio.h>
+
+int main(void) {
+    int x;
+
+    printf("Enter a value for x: ");
+    scanf_s("%d", &x);
+
+    int polynomial = ((((3 * x + 2) * x - 5) * x - 1) * x + 7) * x - 6;
+
+    printf("Solved by x: %d", polynomial);
+}
