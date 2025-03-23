01644293cdb101fafdb8381818e30cf14826471c
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-03-23 19:30:28 -0400

    Change practice

--- c-modern-approach/chapter-3/practice.c
+++ c-modern-approach/chapter-3/practice.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
-    int num1, denom1, num2, denom2, result_num, result_denom;
-
-    printf("Enter first fraction: ");
-    scanf_s("%d/%d", &num1, &denom1);
-
-    printf("Enter second fraction: ");
-    scanf_s("%d/%d", &num2, &denom2);
-
-    result_num = num1 * denom2 + num2 * denom1;
-    result_denom = denom1 * denom2;
-
-    printf("The sum is %d/%d\n", result_num, result_denom);
+    int i, j;
+    float x, y;
+    scanf_s("%f%d%f", &x, &i, &y);
+    printf("%f|%d|%f", x, i, y);
 
     return 0;
 }
