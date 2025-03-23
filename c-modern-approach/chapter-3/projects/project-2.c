7c595d4c113026308afdef51fe5ea9abfa911d76
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-03-23 19:46:59 -0400

    Add code to format purchase information

--- c-modern-approach/chapter-3/projects/project-2.c
+++ c-modern-approach/chapter-3/projects/project-2.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+
+int main(void) {
+    int item, month, day, year;
+    float price;
+
+    printf("Enter item number: ");
+    scanf_s("%d", &item);
+    printf("Enter unit price: ");
+    scanf_s("%f", &price);
+    printf("Enter purchase date (mm/dd/yyyy): ");
+    scanf_s("%d/%d/%d", &month, &day, &year);
+
+    printf("Item\t\tUnit\t\tPurchase\n");
+    printf("\t\tPrice\t\tDate\n");
+    printf("%d\t\t$  %.2f\t%d/%d/%d", item, price, month, day, year);
+}
