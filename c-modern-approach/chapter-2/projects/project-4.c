877688a681390d3ee44d8e3ce213111882685ce1
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-03-22 22:31:15 -0400

    Make program to add 5% tax to any amount

--- c-modern-approach/chapter-2/projects/project-4.c
+++ c-modern-approach/chapter-2/projects/project-4.c
@@ -1,1 +1,10 @@
 #include <stdio.h>
+
+int main(void) {
+    float amount;
+
+    printf("Enter an amount: ");
+    scanf_s("%f", &amount);
+
+    printf("With tax added: %.2f", (amount * 0.05f) + amount);
+}
