669c5134eecbdbffde677ca4c1349b6ce17d293a
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-03-22 21:23:03 -0400

    Change practice to calculate dimensions of a box

--- c-modern-approach/chapter-2/practice.c
+++ c-modern-approach/chapter-2/practice.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("To C, or not to C: that is the question.\n");
+    int weight;
+    int height;
+    int length;
+    int width;
+    int volume;
+
+    printf("Enter height of box: ");
+    scanf_s("%d", &height);
+    printf("Enter length of box: ");
+    scanf_s("%d", &length);
+    printf("Enter width of box: ");
+    scanf_s("%d", &width);
+    volume = height * length * width;
+    weight = (volume + 165) / 166;
+
+    printf("Volume (cubic inches): %d\n", volume);
+    printf("Dimensional weight (pounds): %d\n", weight);
+
+    return 0;
 }
