4dab9538ddd7b53c29cb3da24f4c14c9c4e6845c
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-03-22 23:21:00 -0400

    Add code to calculate amount of bills needed

--- c-modern-approach/chapter-2/projects/project-7.c
+++ c-modern-approach/chapter-2/projects/project-7.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+
+int main(void) {
+    int amount;
+
+    printf("Enter a dollar amount: ");
+    scanf_s("%d", &amount);
+
+    int bill_amount_20 = amount / 20;
+    amount = amount - bill_amount_20 * 20;
+
+    int bill_amount_10 = amount / 10;
+    amount = amount - bill_amount_10 * 10;
+
+    int bill_amount_5 = amount / 5;
+    amount = amount - bill_amount_5 * 5;
+
+    int bill_amount_1 = amount / 1;
+    amount = amount - bill_amount_1 * 1;
+
+    printf("$20 bills: %d\n", bill_amount_20);
+    printf("$10 bills: %d\n", bill_amount_10);
+    printf("$5 bills: %d\n", bill_amount_5);
+    printf("$1 bills: %d\n", bill_amount_1);
+}
