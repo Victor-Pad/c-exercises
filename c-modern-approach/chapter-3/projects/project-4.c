3d13999e0b84c5c03024d110a86805388292de38
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-03-23 22:31:14 -0400

    Add code to format phone number input

--- c-modern-approach/chapter-3/projects/project-4.c
+++ c-modern-approach/chapter-3/projects/project-4.c
@@ -1,1 +1,10 @@
 #include <stdio.h>
+
+int main(void) {
+    int area, prefix, line;
+
+    printf("Enter phone number [(xxx) xxx-xxxx]: ");
+    scanf_s("(%d) %d-%d", &area, &prefix, &line);
+
+    printf("You entered %d.%d.%d", area, prefix, line);
+}
