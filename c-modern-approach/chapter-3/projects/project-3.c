ea5eb058916a3b3bae5108749d745eb69ded4754
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-03-23 22:26:49 -0400

    Add code to divide ISBN's into parts

--- c-modern-approach/chapter-3/projects/project-3.c
+++ c-modern-approach/chapter-3/projects/project-3.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
 
-int main(void) {}
+int main(void) {
+    int gsi, group_identifier, publisher_code, item_number, check_digit;
+
+    printf("Enter ISBN: ");
+    scanf_s("%d-%d-%d-%d-%d", &gsi, &group_identifier, &publisher_code,
+            &item_number, &check_digit);
+
+    printf("GSI Prefix: %d\n", gsi);
+    printf("Group Identifier: %d\n", group_identifier);
+    printf("Publisher code: %d\n", publisher_code);
+    printf("Item number: %d\n", item_number);
+    printf("Check digit: %d\n", check_digit);
+}
