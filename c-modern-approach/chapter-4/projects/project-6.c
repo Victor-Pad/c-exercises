618695ca5814cd36a53d35459286026f6cb13dbc
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-03-30 00:14:14 -0400

    Add code to get the check digit from an EAN

--- c-modern-approach/chapter-4/projects/project-6.c
+++ c-modern-approach/chapter-4/projects/project-6.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+
+int main(void) {
+    int d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, first_sum,
+        second_sum, result;
+
+    printf("Enter the first 12 digits of an EAN: ");
+    scanf_s("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d", &d1, &d2, &d3, &d4, &d5,
+            &d6, &d7, &d8, &d9, &d10, &d11, &d12);
+
+    first_sum = d2 + d4 + d6 + d8 + d10 + d12;
+    second_sum = d1 + d3 + d5 + d7 + d9 + d11;
+
+    result = ((first_sum * 3) + second_sum) - 1;
+    result %= 10;
+    printf("Check digit: %d\n", 9 - result);
+}
