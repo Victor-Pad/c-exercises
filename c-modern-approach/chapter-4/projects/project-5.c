7eb7d01c7a7c5a98cd7fa66dfecb37b95c7db553
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-03-30 00:03:37 -0400

    Add code to check upc

--- c-modern-approach/chapter-4/projects/project-5.c
+++ c-modern-approach/chapter-4/projects/project-5.c
@@ -1,1 +1,44 @@
 #include <stdio.h>
+
+int main(void) {
+    int first_digit, first_group, second_group, first_sum, second_sum, result;
+
+    printf("Enter the first 11 digits of a UPC: ");
+    scanf_s("%1d%5d%5d", &first_digit, &first_group, &second_group);
+
+    first_sum = first_group % 10;
+    first_group /= 10;
+
+    first_sum += first_group % 10;
+    first_group /= 10;
+
+    first_sum += first_group % 10;
+    first_group /= 10;
+
+    first_sum += first_group % 10;
+    first_group /= 10;
+
+    first_sum += first_group % 10;
+
+    first_sum *= 3;
+
+    second_sum = second_group % 10;
+    second_group /= 10;
+
+    second_sum += second_group % 10;
+    second_group /= 10;
+
+    second_sum += second_group % 10;
+    second_group /= 10;
+
+    second_sum += second_group % 10;
+    second_group /= 10;
+
+    second_sum += second_group % 10;
+
+    first_sum += second_sum;
+
+    result = (first_sum - 1) / 10;
+    result = 9 - result;
+    printf("Check digit: %d\n", result);
+}
