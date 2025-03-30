a6eea1876584d73d93264e2921eb30f9c41d0d65
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-03-29 23:48:16 -0400

    Add code to change number from decimal to octal

--- c-modern-approach/chapter-4/projects/project-4.c
+++ c-modern-approach/chapter-4/projects/project-4.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+
+int main(void) {
+    int num, d1, d2, d3, d4, d5;
+
+    printf("Enter a number between 0 and 32767: ");
+    scanf_s("%d", &num);
+
+    d5 = num % 8;
+    num /= 8;
+    d4 = num % 8;
+    num /= 8;
+    d3 = num % 8;
+    num /= 8;
+    d2 = num % 8;
+    num /= 8;
+    d1 = num % 8;
+
+    printf("In octal, your number is: %1d%1d%1d%1d%1d", d1, d2, d3, d4, d5);
+}
