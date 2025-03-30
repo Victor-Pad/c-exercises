3c8ef6b1cd268d1f2359e85a8f919b0ac6fdd95c
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-03-29 23:16:05 -0400

    Add code to reverse a number

--- c-modern-approach/chapter-4/projects/project-1.c
+++ c-modern-approach/chapter-4/projects/project-1.c
@@ -1,1 +1,12 @@
 #include <stdio.h>
+
+int main(void) {
+    int num, reversed_num;
+    printf("Enter a two digit number: ");
+    scanf_s("%d", &num);
+
+    reversed_num = (num % 10) * 10;
+    num /= 10;
+    reversed_num += num % 10;
+    printf("The reversal is: %d\n", reversed_num);
+}
