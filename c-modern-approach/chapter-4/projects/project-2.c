96974cfece883628a7fb92aee5f87752d5891415
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-03-29 23:20:47 -0400

    Add code to reverse three-digit numbers

--- c-modern-approach/chapter-4/projects/project-2.c
+++ c-modern-approach/chapter-4/projects/project-2.c
@@ -1,3 +1,16 @@
 #include <stdio.h>
 
-int main(void) {}
+int main(void) {
+    int num, reversed_num;
+
+    printf("Enter a three-digit number: ");
+    scanf_s("%d", &num);
+
+    reversed_num = (num % 10) * 100;
+    num /= 10;
+    reversed_num += (num % 10) * 10;
+    num /= 10;
+    reversed_num += num;
+
+    printf("The reversal is: %d\n", reversed_num);
+}
