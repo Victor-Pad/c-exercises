28d13c41285bd943d9f295671e13870e580e732b
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-04-02 00:00:57 -0400

    Add code to show the Beaufort scale

--- c-modern-approach/chapter-5/projects/project-4.c
+++ c-modern-approach/chapter-5/projects/project-4.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+
+int main(void) {
+    int speed;
+
+    printf("Enter a wind speed (in knots): ");
+    scanf_s("%d", &speed);
+
+    if (speed < 1)
+        printf("Calm\n");
+    else if (speed <= 3 && speed >= 1)
+        printf("Light air\n");
+    else if (speed <= 27 && speed >= 4)
+        printf("Breeze\n");
+    else if (speed <= 47 && speed >= 28)
+        printf("Gale\n");
+    else if (speed <= 63 && speed >= 48)
+        printf("Storm\n");
+    else
+        printf("Hurricane\n");
+
+    return 0;
+}
