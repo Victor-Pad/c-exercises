782ab0574f41248cbc8fcb3b975c6859201a6d9a
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-04-01 23:55:09 -0400

    Add code to get broker's commissions

--- c-modern-approach/chapter-5/projects/project-3.c
+++ c-modern-approach/chapter-5/projects/project-3.c
@@ -1,1 +1,43 @@
 #include <stdio.h>
+
+int main(void) {
+    int num_shares, price_share;
+    float commission, value;
+
+    printf("Enter number of shares: ");
+    scanf_s("%d", &num_shares);
+
+    printf("Enter the price per share: ");
+    scanf_s("%d", &price_share);
+
+    value = num_shares * price_share;
+
+    if (value < 2500.00f)
+        commission = 30.00f + .017f * value;
+    else if (value < 6250.00f)
+        commission = 56.00f + .0066f * value;
+    else if (value < 20000.00f)
+        commission = 76.00f + .0034f * value;
+    else if (value < 50000.00f)
+        commission = 100.00f + .0022f * value;
+    else if (value < 500000.00f)
+        commission = 155.00f + .0011f * value;
+    else
+        commission = 255.00f + .0009f * value;
+    if (commission < 39.00f)
+        commission = 39.00f;
+
+    printf("Original Broker Commission: $%.2f\n", commission);
+
+    if (num_shares < 2000) {
+        commission = 33.00f + 0.03f * num_shares;
+    } else {
+        commission = 33.00f + 0.02f * num_shares;
+    }
+    if (commission < 39.00f)
+        commission = 39.00f;
+
+    printf("Rival Broker Commission: $%.2f\n", commission);
+
+    return 0;
+}
