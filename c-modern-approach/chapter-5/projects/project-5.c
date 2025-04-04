b0d1b837fff518e3aa571862021c39ec5a53a87e
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-04-03 21:13:59 -0400

    Add code to calculate tax due based on income

--- c-modern-approach/chapter-5/projects/project-5.c
+++ c-modern-approach/chapter-5/projects/project-5.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+
+int main(void) {
+    int income;
+    float tax;
+
+    printf("Enter income: ");
+    scanf_s("%d", &income);
+
+    if (income < 750) {
+        tax = income * 0.01f;
+    } else if (income <= 2250) {
+        tax = 7.50f + (income * 0.02f);
+    } else if (income <= 3750) {
+        tax = 37.50f + (income * 0.03f);
+    } else if (income <= 5250) {
+        tax = 82.50f + (income * 0.04f);
+    } else if (income <= 7000) {
+        tax = 142.50f + (income * 0.05f);
+    } else if (income > 7000) {
+        tax = 230.00f + (income * 0.06f);
+    }
+
+    printf("Tax due: %.2f", tax);
+}
