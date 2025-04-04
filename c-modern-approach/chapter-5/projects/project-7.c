b6649bf290832b204c34fb2378d8cf28f563f240
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-04-03 21:52:36 -0400

    Add code to find smallest and largest integers

--- c-modern-approach/chapter-5/projects/project-7.c
+++ c-modern-approach/chapter-5/projects/project-7.c
@@ -1,1 +1,39 @@
 #include <stdio.h>
+
+int main(void) {
+    int first, second, third, fourth, largest, smallest;
+
+    printf("Enter four integers: ");
+    scanf_s("%d %d %d %d", &first, &second, &third, &fourth);
+
+    largest = first;
+    smallest = fourth;
+
+    if (first < second && first < third && first < fourth) {
+        smallest = first;
+    } else if (second < first && second < third && second < fourth) {
+        smallest = second;
+    } else {
+        smallest = third;
+    }
+
+    if (second > first && second > third && second > fourth) {
+        largest = second;
+    } else if (third > first && third > second && third > fourth) {
+        largest = third;
+    } else {
+        largest = fourth;
+    }
+
+    if (largest < first) {
+        largest = first;
+    }
+    if (smallest > fourth) {
+        smallest = fourth;
+    }
+
+    printf("Largest: %d\n", largest);
+    printf("Smallest: %d\n", smallest);
+
+    return 0;
+}
