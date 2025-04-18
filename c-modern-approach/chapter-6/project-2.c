4790648b5daed0d995388a633b31dee3d715d96e
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-04-18 01:37:06 -0400

    Add code to check for greatest common divisor

--- c-modern-approach/chapter-6/project-2.c
+++ c-modern-approach/chapter-6/project-2.c
@@ -1,3 +1,16 @@
 #include <stdio.h>
 
-int main(void) {}
+int main(void) {
+    int remainder, n, m;
+
+    printf("Enter two integers: ");
+    scanf_s("%d %d", &n, &m);
+
+    while (n != 0) {
+        remainder = m % n;
+        m = n;
+        n = remainder;
+    }
+
+    printf("Greatest common divisor: %d", m);
+}
