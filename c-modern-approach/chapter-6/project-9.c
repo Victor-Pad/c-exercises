b156396a0ac751fda607b1c46f26a24b8f077766
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-04-18 03:06:52 -0400

    Add code to check for balance

--- c-modern-approach/chapter-6/project-9.c
+++ c-modern-approach/chapter-6/project-9.c
@@ -2,6 +2,7 @@
 
 int main(void) {
     float amount_of_loan, interest_rate, monthly_pay;
+    int num_of_payments;
 
     printf("Enter amount of loan: ");
     scanf_s("%f", &amount_of_loan);
@@ -10,18 +11,16 @@ int main(void) {
     printf("Enter monthly payment: ");
     scanf_s("%f", &monthly_pay);
 
+    printf("Enter number of payments: ");
+    scanf_s("%d", &num_of_payments);
+
     float balance, interest_amount;
 
     balance = amount_of_loan;
-    interest_amount = ((interest_rate * (balance / 100)) / 12);
-    balance = (balance - monthly_pay) + interest_amount;
-    printf("Balance remaining after first payment: $%.2f\n", balance);
 
-    interest_amount = ((interest_rate * (balance / 100)) / 12);
-    balance = (balance - monthly_pay) + interest_amount;
-    printf("Balance remaining after second payment: $%.2f\n", balance);
-
-    interest_amount = ((interest_rate * (balance / 100)) / 12);
-    balance = (balance - monthly_pay) + interest_amount;
-    printf("Balance remaining after third payment: $%.2f\n", balance);
+    for (int i = 1; i <= num_of_payments; i++) {
+        interest_amount = ((interest_rate * (balance / 100)) / 12);
+        balance = (balance - monthly_pay) + interest_amount;
+        printf("Balance remaining after the payment: $%.2f\n", balance);
+    }
 }
