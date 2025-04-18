0e556cb9f206e6da85b594a0fa40dc5f74764d4f
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-04-17 23:43:48 -0400

    Add practice code

--- c-modern-approach/chapter-6/practice.c
+++ c-modern-approach/chapter-6/practice.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
 
-int main(void) {}
+int main(void) {
+    int x, i = 0;
+
+    printf("This program prints a table of squares\n");
+    printf("Enter number of entries in table: ");
+    scanf_s("%d", &x);
+
+    while (i < x) {
+        i += 1;
+
+        printf("%10d%10d\n", i, i * i);
+    }
+}
