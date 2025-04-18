6163c56424f7ea32643e0a6e1afd77a514c04725
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-04-18 01:23:19 -0400

    Add code to look for largest number

--- c-modern-approach/chapter-6/project-1.c
+++ c-modern-approach/chapter-6/project-1.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+
+int main(void) {
+    float num, largest;
+
+    printf("Enter a number: ");
+    scanf_s("%f", &num);
+    largest = num;
+    do {
+        printf("Enter a number: ");
+        scanf_s("%f", &num);
+
+        if (largest < num) {
+            largest = num;
+        }
+    } while (num > 0);
+
+    printf("The largest number entered was %.2f", largest);
+}
