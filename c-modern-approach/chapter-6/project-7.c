fbab29878f3924f32a9eba4ce7fbe8632bf72cc5
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-04-18 02:27:23 -0400

    Add file and code for the 7th project

---
+++ c-modern-approach/chapter-6/project-7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+    int i, n, odd, square;
+
+    printf("This program prints a table of squares.\n");
+    printf("Enter number of entries in table: ");
+    scanf_s("%d", &n);
+
+    odd = 3;
+
+    for (i = 1, square = 1; i <= n; odd += 2, ++i) {
+        printf("%10d%10d\n", i, square);
+    }
+
+    return 0;
+}
