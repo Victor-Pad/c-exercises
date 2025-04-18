909d7fb779957a505035cd29b0d34c4222dcf414
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-04-18 02:23:36 -0400

    Add code to print even squares between i and n

--- c-modern-approach/chapter-6/project-6.c
+++ c-modern-approach/chapter-6/project-6.c
@@ -1,3 +1,14 @@
 #include <stdio.h>
 
-int main(void) {}
+int main(void) {
+    int n;
+
+    printf("Enter a number: ");
+    scanf_s("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 == 0 && i * i <= n) {
+            printf("%d\n", i * i);
+        }
+    }
+}
