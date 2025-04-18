54886bdb2cb1743f380b4c994a26dfa7c2af19c2
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-04-18 01:46:55 -0400

    Add code to check for lowest terms of fractions

--- c-modern-approach/chapter-6/project-3.c
+++ c-modern-approach/chapter-6/project-3.c
@@ -1,3 +1,19 @@
 #include <stdio.h>
 
-int main(void) {}
+int main(void) {
+    int numerator, denominator, remainder, gcd, n;
+
+    printf("Enter a fraction: ");
+    scanf_s("%d/%d", &numerator, &denominator);
+
+    gcd = denominator;
+    n = numerator;
+
+    while (n != 0) {
+        remainder = n % gcd;
+        gcd = n;
+        n = remainder;
+    }
+
+    printf("In lowest terms: %d/%d\n", numerator / gcd, denominator / gcd);
+}
