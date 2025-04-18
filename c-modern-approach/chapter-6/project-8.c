cf0e634b5fb3bbc671d2375bd40d75ab0316e4a4
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-04-18 02:51:02 -0400

    Add code to print a calendar

--- c-modern-approach/chapter-6/project-8.c
+++ c-modern-approach/chapter-6/project-8.c
@@ -1,3 +1,22 @@
 #include <stdio.h>
 
-int main(void) {}
+int main(void) {
+    int month, day;
+
+    printf("Enter number of days in month: ");
+    scanf_s("%d", &month);
+    printf("Enter starting day of the week: (1=Sun, 7=Sat): ");
+    scanf_s("%d", &day);
+
+    for (int i = 1; i < day; i++) {
+        printf("  ");
+    }
+
+    for (int i = 1; i <= month; i++) {
+        printf("%3d", i);
+
+        if ((day + i - 1) % 7 == 0) {
+            printf("\n");
+        }
+    }
+}
