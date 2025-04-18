c3e813cb8c5249eddfa3c98512614a5998214b67
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-04-18 02:18:20 -0400

    Add code to reverse integers of any length

--- c-modern-approach/chapter-6/project-5.c
+++ c-modern-approach/chapter-6/project-5.c
@@ -1,3 +1,16 @@
 #include <stdio.h>
 
-int main(void) {}
+int main(void) {
+    int num, reversed_num, last_digit;
+
+    printf("Enter a integer: ");
+    scanf_s("%d", &num);
+
+    do {
+        last_digit = (num % 10);
+        num /= 10;
+        reversed_num = reversed_num * 10 + last_digit;
+    } while (num != 0);
+
+    printf("The reversal is: %d\n", reversed_num);
+}
