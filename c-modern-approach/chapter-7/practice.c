a656be6ae3aaee06001d4a1a9ed2429574605776
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-04-25 01:45:58 -0400

    Add practice code to sum numbers

--- c-modern-approach/chapter-7/practice.c
+++ c-modern-approach/chapter-7/practice.c
@@ -1,3 +1,16 @@
 #include <stdio.h>
 
-int main(void) {}
+int main(void) {
+    long n, sum = 0;
+
+    printf("This program sums a series of integers\n");
+
+    printf("Enter integers (0 to terminate): ");
+
+    scanf_s("%ld", &n);
+    while (n != 0) {
+        sum += n;
+        scanf_s("%ld", &n);
+    }
+    printf("The sum is: %ld", sum);
+}
