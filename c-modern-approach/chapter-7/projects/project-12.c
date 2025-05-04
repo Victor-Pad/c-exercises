94a57f4f1d86679aa090b00d78241ad5d8c042aa
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-05-04 18:47:14 -0400

    Add code for the 12th project

--- c-modern-approach/chapter-7/projects/project-12.c
+++ c-modern-approach/chapter-7/projects/project-12.c
@@ -1,3 +1,30 @@
 #include <stdio.h>
 
-int main(void) {}
+int main(void) {
+    char ch;
+    float operand, result;
+
+    printf("Enter an expression: ");
+    scanf_s("%f", &operand);
+    result = operand;
+
+    while ((ch = getchar()) != '\n') {
+        scanf_s("%f", &operand);
+
+        switch (ch) {
+            case '+':
+                result += operand;
+                break;
+            case '*':
+                result *= operand;
+                break;
+            case '-':
+                result -= operand;
+                break;
+            case '/':
+                result /= operand;
+                break;
+        }
+    }
+    printf("Value of expression: %.1f", result);
+}
