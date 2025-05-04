fc10cc6595d31980f53dcd8f27d3d6091136ed9e
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-05-04 03:49:59 -0400

    Add code for the 4th project

--- c-modern-approach/chapter-7/projects/project-4.c
+++ c-modern-approach/chapter-7/projects/project-4.c
@@ -1,3 +1,29 @@
 #include <stdio.h>
 
-int main(void) {}
+int main(void) {
+    char ch;
+
+    printf("Enter phone number: ");
+
+    while ((ch = getchar()) != '\n') {
+        if (ch == 'A' || ch == 'B' || ch == 'C') {
+            printf("2");
+        } else if (ch == 'D' || ch == 'E' || ch == 'F') {
+            printf("3");
+        } else if (ch == 'G' || ch == 'H' || ch == 'I') {
+            printf("4");
+        } else if (ch == 'J' || ch == 'K' || ch == 'L') {
+            printf("5");
+        } else if (ch == 'M' || ch == 'N' || ch == 'O') {
+            printf("6");
+        } else if (ch == 'P' || ch == 'R' || ch == 'S') {
+            printf("7");
+        } else if (ch == 'T' || ch == 'U' || ch == 'V') {
+            printf("8");
+        } else if (ch == 'W' || ch == 'X' || ch == 'Y') {
+            printf("9");
+        } else {
+            printf("%c", ch);
+        }
+    }
+}
