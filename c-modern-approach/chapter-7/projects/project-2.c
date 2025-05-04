238adda813a1130ac8630c38df3b5da82a584e43
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-05-04 03:26:25 -0400

    Add code for the 2nd project

--- c-modern-approach/chapter-7/projects/project-2.c
+++ c-modern-approach/chapter-7/projects/project-2.c
@@ -1,3 +1,24 @@
 #include <stdio.h>
 
-int main(void) {}
+int main(void) {
+    long int i;
+    int n;
+    char ch;
+
+    printf("This program prints a table of squares.\n");
+    printf("Enter number of entries in table: ");
+
+    scanf_s("%d", &n);
+    ch = getchar();
+
+    for (i = 1; i <= n; i++) {
+        printf("%ld \t %ld\n", i, i * i);
+
+        if (i % 24 == 0) {
+            printf("\nPress Enter to continue... ");
+            ch = getchar();
+        }
+    }
+
+    return 0;
+}
