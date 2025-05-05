c6a6f8797491033f2fc1db999a0261c714791b3f
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-05-04 20:08:50 -0400

    Add code for the 14th project

--- c-modern-approach/chapter-7/projects/project-14.c
+++ c-modern-approach/chapter-7/projects/project-14.c
@@ -1,3 +1,18 @@
+#include <math.h>
 #include <stdio.h>
 
-int main(void) {}
+int main(void) {
+    int x;
+    double diff, y = 1.0f, average;
+
+    printf("Enter a positive number: ");
+    scanf_s("%d", &x);
+
+    do {
+        average = (y + (x / y)) / 2;
+        diff = fabs(average - y);
+        y = average;
+    } while (diff > y * .00001);
+
+    printf("Square root: %lf", average);
+}
