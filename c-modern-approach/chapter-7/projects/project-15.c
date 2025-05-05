b64adc4d48140c344f36d6d6c013ef60898808ec
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-05-04 20:17:03 -0400

    Add code for the 15th project

--- c-modern-approach/chapter-7/projects/project-15.c
+++ c-modern-approach/chapter-7/projects/project-15.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
 
-int main(void) {}
+int main(void) {
+    int x, factorial;
+
+    printf("Enter a positive number: ");
+    scanf_s("%d", &x);
+    factorial = x;
+
+    for (int i = x - 1; i > 1; i--) {
+        factorial *= i;
+    }
+
+    printf("Factorial of %d: %d", x, factorial);
+}
