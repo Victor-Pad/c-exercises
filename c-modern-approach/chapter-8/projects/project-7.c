6b514d07055c119a695bc9924c1ae9acc7c86871
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-06-05 03:42:35 -0400

    Add code for the 7th project

--- c-modern-approach/chapter-8/projects/project-7.c
+++ c-modern-approach/chapter-8/projects/project-7.c
@@ -2,19 +2,34 @@
 
 int main(void) {
     int table[5][5] = {0};
-    int num = 0;
+    int num, sum_col = 0, sum_row = 0;
 
     for (int i = 0; i < 5; i++) {
-        printf("Enter row %d: ", i);
+        printf("Enter row %d: ", i + 1);
         for (int j = 0; j < 5; j++) {
             scanf_s("%d", &num);
             table[i][j] = num;
         }
     }
 
+    printf("Row totals: ");
     for (int i = 0; i < 5; i++) {
+        sum_row = 0;
         for (int j = 0; j < 5; j++) {
-            printf("Table: %d", table[i][j]);
+            sum_row += table[i][j];
         }
+
+        printf("%d ", sum_row);
     }
+
+    printf("\nColumn totals: ");
+    for (int i = 0; i < 5; i++) {
+        sum_col = 0;
+        for (int j = 0; j < 5; j++) {
+            sum_col += table[j][i];
+        }
+
+        printf("%d ", sum_col);
+    }
+    printf("\n");
 }
