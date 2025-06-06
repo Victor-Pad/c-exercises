8a34bf669b2618984bfaad4563fb958b75d4995f
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-06-06 02:18:00 -0400

    Add file for the 9th project

---
+++ c-modern-approach/chapter-8/projects/project-9.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    char grid[10][10] = {'.'};
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            printf("%c", grid[i][j]);
+        }
+    }
+}
