f5dc1df2eee75e9ff58771c763fb4b86a11383bb
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-06-07 07:50:57 -0400

    Add code for the 17th project

--- c-modern-approach/chapter-8/projects/project-17.c
+++ c-modern-approach/chapter-8/projects/project-17.c
@@ -1,7 +1,8 @@
+#include <stdbool.h>
 #include <stdio.h>
 
 int main(void) {
-    int size = 0, col = 0;
+    int size = 0, row = 0;
 
     printf("This program creates a magic square of a specified size.\n");
     printf("The size must be an odd number between 1 and 99.\n");
@@ -9,11 +10,34 @@ int main(void) {
     scanf_s("%d", &size);
 
     int magic_square[size][size];
-    magic_square[0][size / 2] = 1;
 
-    for (int i = 1; i <= size * size; i++) {
-        if (i - 1 > size) {
-            // size - 1;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            magic_square[i][j] = 0;
         }
+    }
+
+    magic_square[0][size / 2] = 1;
+    int col = (size / 2);
+
+    for (int num = 1; num <= size * size; num++) {
+        magic_square[row][col] = num;
+
+        int next_row = (row - 1 + size) % size;
+        int next_col = (col + 1) % size;
+
+        if (magic_square[next_row][next_col] != 0) {
+            row = (row + 1) % size;
+        } else {
+            row = next_row;
+            col = next_col;
+        }
+    }
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            printf("%3d", magic_square[i][j]);
+        }
+        printf("\n");
     }
 }
