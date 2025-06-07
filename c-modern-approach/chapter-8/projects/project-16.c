a7e4cd9c66e24142125344ef3521e19ac89fe458
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-06-07 06:37:24 -0400

    Add code for the 16th project

--- c-modern-approach/chapter-8/projects/project-16.c
+++ c-modern-approach/chapter-8/projects/project-16.c
@@ -1,4 +1,32 @@
 #include <ctype.h>
 #include <stdio.h>
 
-int main(void) {}
+int main(void) {
+    char ch;
+    int anagram = 0, word[26] = {0};
+
+    printf("Enter first word: ");
+    while ((ch = getchar()) != '\n') {
+        if (isalpha(ch))
+            word[tolower(ch) - 'a']++;
+    }
+
+    printf("Enter second word: ");
+    while ((ch = getchar()) != '\n') {
+        if (isalpha(ch))
+            word[tolower(ch) - 'a']--;
+    }
+
+    for (int i = 0; i < 26; i++) {
+        if (word[i] != 0) {
+            anagram = 1;
+            break;
+        }
+    }
+
+    if (anagram == 1)
+        printf("The words are not anagrams.");
+    else {
+        printf("The words are anagrams.");
+    }
+}
