6a44b23c1c30f18af26949d998b2c440c8b8a95e
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-06-07 06:11:31 -0400

    Add code for the 15th project

--- c-modern-approach/chapter-8/projects/project-15.c
+++ c-modern-approach/chapter-8/projects/project-15.c
@@ -1,3 +1,26 @@
+#include <ctype.h>
 #include <stdio.h>
 
-int main(void) {}
+int main(void) {
+    int index = 0, shift_amount = 0;
+    char ch, message[80];
+
+    printf("Enter message to be encrypted: ");
+    while ((ch = getchar()) != '\n') {
+        message[index] = ch;
+        index++;
+    }
+
+    printf("Enter shift amount (1-25): ");
+    scanf_s("%d", &shift_amount);
+
+    printf("Encrypted message: ");
+    for (int i = 0; i < index; i++) {
+        if (isupper(message[i]))
+            putchar(((message[i] - 'A' + shift_amount) % 26) + 'A');
+        else if (islower(message[i]))
+            putchar(((message[i] - 'a' + shift_amount) % 26) + 'a');
+        else
+            putchar(message[i]);
+    }
+}
