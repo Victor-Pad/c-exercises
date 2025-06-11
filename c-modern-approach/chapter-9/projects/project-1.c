b017fa2a76cd6f90ea933650b55fe195bed10dd7
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-06-11 04:04:33 -0400

    Add code for the 1st project

--- c-modern-approach/chapter-9/projects/project-1.c
+++ c-modern-approach/chapter-9/projects/project-1.c
@@ -1,3 +1,49 @@
 #include <stdio.h>
 
-int main(void) {}
+void selection_sort(int n, int[]);
+
+int main(void) {
+    int n = 10, numbers[10];
+
+    printf("Enter a series of 10 integers: ");
+    for (int i = 0; i < 10; i++) {
+        scanf_s("%d", &n);
+        numbers[i] = n;
+    }
+
+    printf("Array before selection sort: ");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", numbers[i]);
+    }
+    printf("\n");
+
+    selection_sort(10, numbers);
+
+    printf("Array after selection sort: ");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", numbers[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
+void selection_sort(int n, int numbers[n]) {
+    int largest_index, largest = numbers[0];
+
+    if (n <= 1)
+        return;
+
+    for (int i = 0; i < n; i++) {
+        if (largest < numbers[i]) {
+            largest = numbers[i];
+            largest_index = i;
+        }
+    }
+
+    numbers[largest_index] = numbers[n - 1];
+    numbers[n - 1] = largest;
+    n--;
+
+    selection_sort(n, numbers);
+}
