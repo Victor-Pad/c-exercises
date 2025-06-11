eaa80b070ecff9b0ad1b9b9787475beb6e6e6361
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-06-11 19:46:20 -0400

    Reformat old proejct

--- c-modern-approach/chapter-9/projects/project-5.c
+++ c-modern-approach/chapter-9/projects/project-5.c
@@ -1,8 +1,11 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+void create_magic_square(int n, int magic_square[n][n]);
+void print_magic_square(int n, int magic_square[n][n]);
+
 int main(void) {
-    int size = 0, row = 0;
+    int size;
 
     printf("This program creates a magic square of a specified size.\n");
     printf("The size must be an odd number between 1 and 99.\n");
@@ -17,25 +20,33 @@ int main(void) {
         }
     }
 
-    magic_square[0][size / 2] = 1;
-    int col = (size / 2);
+    create_magic_square(size, magic_square);
+    print_magic_square(size, magic_square);
+}
 
-    for (int num = 1; num <= size * size; num++) {
+void create_magic_square(int n, int magic_square[n][n]) {
+    int row = 0;
+    magic_square[0][n / 2] = 1;
+    int col = (n / 2);
+
+    for (int num = 1; num <= n * n; num++) {
         magic_square[row][col] = num;
 
-        int next_row = (row - 1 + size) % size;
-        int next_col = (col + 1) % size;
+        int next_row = (row - 1 + n) % n;
+        int next_col = (col + 1) % n;
 
         if (magic_square[next_row][next_col] != 0) {
-            row = (row + 1) % size;
+            row = (row + 1) % n;
         } else {
             row = next_row;
             col = next_col;
         }
     }
+}
 
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
+void print_magic_square(int n, int magic_square[n][n]) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             printf("%3d", magic_square[i][j]);
         }
         printf("\n");
