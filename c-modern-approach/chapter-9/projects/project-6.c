402df6ec0f0563bffe490c291121a513351e7f8f
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-06-11 20:38:49 -0400

    Add code for the 6th project

--- c-modern-approach/chapter-9/projects/project-6.c
+++ c-modern-approach/chapter-9/projects/project-6.c
@@ -1,3 +1,17 @@
 #include <stdio.h>
 
-int main(void) {}
+int compute_formula(int x);
+int main(void) {
+    int x;
+
+    printf("Please enter a number for x: ");
+    scanf_s("%d", &x);
+
+    int result = compute_formula(x);
+    printf("Result: %d", result);
+}
+
+int compute_formula(int x) {
+    return 3 * (x * x * x * x * x) + 2 * (x * x * x * x) - 5 * (x * x * x) -
+           (x * x) + 7 * x - 6;
+}
