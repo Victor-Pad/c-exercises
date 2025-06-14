5f6d41cded15bf143bb40787eab17cc897c12570
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-06-14 03:53:07 -0400

    Add code for the poker.c practice program

--- c-modern-approach/chapter-10/practice.c
+++ c-modern-approach/chapter-10/practice.c
@@ -1,62 +1,220 @@
-/* Asks user to guess a hidden number */
+/* Classifies a poker hand */
+#include <stdbool.h> /* C99 only */
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
-#define MAX_NUMBER 100
-/* external variable */
-int secret_number;
+#define NUM_RANKS 13
+#define NUM_SUITS 4
+#define NUM_CARDS 5
+
+/* external variables */
+int num_in_rank[NUM_RANKS];
+int num_in_suit[NUM_SUITS];
+bool straight, flush, four, three;
+int pairs; /* can be 0, 1, or 2 */
+
 /* prototypes */
-void initialize_number_generator(void);
-int new_secret_number(void);
-void read_guesses(int secret_number);
+void read_cards(void);
+void analyze_hand(void);
+void print_result(void);
+
+/********************************************************** * main: Calls
+ * read_cards, analyze_hand, and print_result * * repeatedly. *
+ * **********************************************************/
 
 int main(void) {
-    char command;
-    int secret_number;
-
-    printf("Guess the secret number between 1 and %d.\n\n", MAX_NUMBER);
-    initialize_number_generator();
-    do {
-        secret_number = new_secret_number();
-        printf("A new number has been chosen.\n");
-        read_guesses(secret_number);
-        printf("Play again? (Y/N) ");
-        scanf_s(" %c", &command);
-        printf("\n");
-    } while (command == 'y' || command == 'Y');
-
-    return 0;
-} /********************************************************** *
-    new_secret_number: Returns a randomly chosen number * * between 1 and
-    MAX_NUMBER.
-    **********************************************************/
-void initialize_number_generator(void) {
-    srand((unsigned)time(NULL));
-} /********************************************************** *
-     choose_new_secret_number: Randomly selects a number * * between 1 and
-     MAX_NUMBER and * * stores it in secret_number. *
-     **********************************************************/
-int new_secret_number(void) {
-    return rand() % MAX_NUMBER + 1;
-} /********************************************************** * read_guesses:
-     Repeatedly reads user guesses and tells * * the user whether each guess is
-     too low, * * too high, or correct. When the guess is * * correct, prints
-     the total number of * * guesses and returns. *
-     **********************************************************/
-void read_guesses(int secret_number) {
-    int guess, num_guesses = 0;
-
     for (;;) {
-        num_guesses++;
-        printf("Enter guess: ");
-        scanf_s("%d", &guess);
-        if (guess == secret_number) {
-            printf("You won in %d guesses!\n\n", num_guesses);
-            return;
-        } else if (guess < secret_number)
-            printf("Too low; try again.\n");
-        else
-            printf("Too high; try again.\n");
+        read_cards();
+        analyze_hand();
+        print_result();
     }
+}
+
+/********************************************************** * read_cards:
+     Reads the cards into the external * * variables num_in_rank and
+     num_in_suit; * * checks for bad cards and duplicate cards. *
+     **********************************************************/
+
+void read_cards(void) {
+    bool card_exists[NUM_RANKS][NUM_SUITS];
+    char ch, rank_ch, suit_ch;
+    int rank, suit;
+    bool bad_card;
+    int cards_read = 0;
+
+    for (rank = 0; rank < NUM_RANKS; rank++) {
+        num_in_rank[rank] = 0;
+        for (suit = 0; suit < NUM_SUITS; suit++)
+            card_exists[rank][suit] = false;
+    }
+
+    for (suit = 0; suit < NUM_SUITS; suit++)
+        num_in_suit[suit] = 0;
+
+    while (cards_read < NUM_CARDS) {
+        bad_card = false;
+
+        printf("Enter a card: ");
+        rank_ch = getchar();
+        switch (rank_ch) {
+            case '0':
+                exit(EXIT_SUCCESS);
+            case '2':
+                rank = 0;
+                break;
+            case '3':
+                rank = 1;
+                break;
+            case '4':
+                rank = 2;
+                break;
+            case '5':
+                rank = 3;
+                break;
+            case '6':
+                rank = 4;
+                break;
+            case '7':
+                rank = 5;
+                break;
+            case '8':
+                rank = 6;
+                break;
+            case '9':
+                rank = 7;
+                break;
+            case 't':
+            case 'T':
+                rank = 8;
+                break;
+            case 'j':
+            case 'J':
+                rank = 9;
+                break;
+            case 'q':
+            case 'Q':
+                rank = 10;
+                break;
+            case 'k':
+            case 'K':
+                rank = 11;
+                break;
+            case 'a':
+            case 'A':
+                rank = 12;
+                break;
+            default:
+                bad_card = true;
+        }
+
+        suit_ch = getchar();
+        switch (suit_ch) {
+            case 'c':
+            case 'C':
+                suit = 0;
+                break;
+            case 'd':
+            case 'D':
+                suit = 1;
+                break;
+            case 'h':
+            case 'H':
+                suit = 2;
+                break;
+            case 's':
+            case 'S':
+                suit = 3;
+                break;
+            default:
+                bad_card = true;
+        }
+
+        while ((ch = getchar()) != '\n')
+            if (ch != ' ')
+                bad_card = true;
+
+        if (bad_card)
+            printf("Bad card; ignored.\n");
+        else if (card_exists[rank][suit])
+            printf("Duplicate card; ignored.\n");
+        else {
+            num_in_rank[rank]++;
+            num_in_suit[suit]++;
+            card_exists[rank][suit] = true;
+            cards_read++;
+        }
+    }
+}
+
+/********************************************************** * analyze_hand:
+     Determines whether the hand contains a * * straight, a flush,
+     four-of-a-kind, * * and/or three-of-a-kind; determines the * * number of
+     pairs; stores the results into * * the external variables straight, flush,
+     * * four, three, and pairs. *
+     **********************************************************/
+
+void analyze_hand(void) {
+    int num_consec = 0;
+    int rank, suit;
+
+    straight = false;
+    flush = false;
+    four = false;
+    three = false;
+    pairs = 0; /* check for flush */
+
+    for (suit = 0; suit < NUM_SUITS; suit++)
+        if (num_in_suit[suit] == NUM_CARDS)
+            flush = true; /* check for straight */
+
+    rank = 0;
+    while (num_in_rank[rank] == 0)
+        rank++;
+
+    for (; rank < NUM_RANKS && num_in_rank[rank] > 0; rank++)
+        num_consec++;
+
+    if (num_consec == NUM_CARDS) {
+        straight = true;
+        return;
+    }
+
+    /* check for 4-of-a-kind, 3-of-a-kind, and pairs */
+    for (rank = 0; rank < NUM_RANKS; rank++) {
+        if (num_in_rank[rank] == 4)
+            four = true;
+        if (num_in_rank[rank] == 3)
+            three = true;
+        if (num_in_rank[rank] == 2)
+            pairs++;
+    }
+}
+
+/********************************************************** *
+ * print_result: Prints the classification of the hand,
+ *
+ *  based on the values of the
+     external * * variables straight, flush, four, three, * * and pairs. *
+     **********************************************************/
+
+void print_result(void) {
+    if (straight && flush)
+        printf("Straight flush");
+    else if (four)
+        printf("Four of a kind");
+    else if (three && pairs == 1)
+        printf("Full house");
+    else if (flush)
+        printf("Flush");
+    else if (straight)
+        printf("Straight");
+    else if (three)
+        printf("Three of a kind");
+    else if (pairs == 2)
+        printf("Two pairs");
+    else if (pairs == 1)
+        printf("Pair");
+    else
+        printf("High card");
+    printf("\n\n");
 }
