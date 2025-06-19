913c60a0a4c8648e97db9b784c0c749529b072eb
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-06-19 04:01:59 -0400

    Add code for the 3rd project

--- c-modern-approach/chapter-11/projects/project-3.c
+++ c-modern-approach/chapter-11/projects/project-3.c
@@ -1,3 +1,40 @@
 #include <stdio.h>
 
-int main(void) {}
+void reduce(int numerator,
+            int denominator,
+            int *reduced_numerator,
+            int *reduced_denominator);
+int main(void) {
+    int numerator, denominator, reduced_num, reduced_den;
+
+    printf("Enter a fraction: ");
+    scanf_s("%d/%d", &numerator, &denominator);
+
+    reduce(numerator, denominator, &reduced_num, &reduced_den);
+    printf("In lowest terms: %d/%d\n", reduced_num, reduced_den);
+}
+
+void reduce(int numerator,
+            int denominator,
+            int *reduced_numerator,
+            int *reduced_denominator) {
+    int a = numerator;
+    int b = denominator;
+    int temp;
+
+    if (a < 0)
+        a = -a;
+    if (b < 0)
+        b = -b;
+
+    while (b != 0) {
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    int gcd = a;
+
+    *reduced_numerator = numerator / gcd;
+    *reduced_denominator = denominator / gcd;
+}
