6a8b6168131ebd38f8d3e1f122e8bfe910c736dd
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-06-20 06:55:21 -0400

    Add code for the 4th project

--- c-modern-approach/chapter-12/projects/project-4.c
+++ c-modern-approach/chapter-12/projects/project-4.c
@@ -1,4 +1,30 @@
 #include <ctype.h>
 #include <stdio.h>
 
-int main(void) {}
+#define SEN_LENGTH 100
+
+int main(void) {
+    char ch, sentence[SEN_LENGTH], *ptr_end, *ptr_start;
+
+    printf("Enter a message: ");
+    for (ptr_end = sentence; ptr_end < sentence + SEN_LENGTH;) {
+        ch = getchar();
+
+        if (ch == '\n')
+            break;
+        if (isalpha(ch))
+            *ptr_end++ = tolower(ch);
+    }
+
+    ptr_end--;
+
+    for (ptr_start = sentence; ptr_end > sentence; ptr_end--, ptr_start++) {
+        if (*ptr_start != *ptr_end) {
+            printf("Not a palindrome\n");
+            return 0;
+        }
+    }
+
+    printf("Palindrome\n");
+    return 0;
+}
