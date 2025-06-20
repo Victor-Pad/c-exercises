f712230144fb64ef9ab29c371405020077fa5e5a
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-06-20 05:27:53 -0400

    Add code for the 1st project

--- c-modern-approach/chapter-12/projects/project-1.c
+++ c-modern-approach/chapter-12/projects/project-1.c
@@ -1,3 +1,22 @@
 #include <stdio.h>
 
-int main(void) {}
+#define N 100
+int main(void) {
+    char *p;
+    char ch, message[N];
+
+    p = message;
+
+    printf("Enter a message: ");
+    while ((ch = getchar()) != '\n' && p < message + N) {
+        *p++ = ch;
+    }
+
+    printf("Reversal is: ");
+    while (p > message) {
+        p--;
+        printf("%c", *p);
+    }
+
+    return 0;
+}
