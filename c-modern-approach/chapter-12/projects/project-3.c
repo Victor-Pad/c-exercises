f31108eca0482d3f791336adbfe59e1fc9948897
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-06-20 06:23:27 -0400

    Add code for the 3rd project

--- c-modern-approach/chapter-12/projects/project-3.c
+++ c-modern-approach/chapter-12/projects/project-3.c
@@ -2,4 +2,20 @@
 
 #define MSG_LENGTH 100
 
-int main(void) {}
+int main(void) {
+    char msg[MSG_LENGTH], *p;
+
+    printf("Enter a message: ");
+    for (p = msg; p < msg + MSG_LENGTH; p++) {
+        *p = getchar();
+        if (*p == '\n')
+            break;
+    }
+
+    printf("Reversal is: ");
+    for (p--; p >= msg; p--)
+        putchar(*p);
+    putchar('\n');
+
+    return 0;
+}
