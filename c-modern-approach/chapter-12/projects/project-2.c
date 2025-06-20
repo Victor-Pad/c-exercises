5eb2e242d0e7984eafb712f99007360e5880905b
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-06-20 06:20:18 -0400

    Add code for the 2nd project

--- c-modern-approach/chapter-12/projects/project-2.c
+++ c-modern-approach/chapter-12/projects/project-2.c
@@ -1,3 +1,26 @@
+#include <ctype.h>
 #include <stdio.h>
 
-int main(void) {}
+int main(void) {
+    char ch, sentence[100], *ptr_start = sentence, *ptr_end = sentence;
+
+    printf("Enter a message: ");
+    while ((ch = getchar()) != '\n') {
+        if (isalpha(ch))
+            *ptr_end++ = tolower(ch);
+    }
+
+    ptr_end -= 1;
+
+    while (ptr_end > sentence) {
+        if (*ptr_start != *ptr_end) {
+            printf("Not a palindrome\n");
+            return 0;
+        }
+        ptr_start++;
+        ptr_end--;
+    }
+
+    printf("Palindrome");
+    return 0;
+}
