e181f8079be74f24ab40f4378e3a7cf6f8da89b5
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-06-20 05:09:32 -0400

    Make changes

--- c-modern-approach/chapter-12/practice.c
+++ c-modern-approach/chapter-12/practice.c
@@ -2,18 +2,4 @@
 
 #define N 10
 
-int main(void) {
-    int a[N], *p;
-
-    printf("Enter %d numbers: ", N);
-    for (p = a; p < a + N; p++)
-        scanf_s("%d", p);
-
-    printf("In reverse order: ");
-    for (p = a + N - 1; p >= a; p--)
-        printf(" %d", *p);
-
-    printf("\n");
-
-    return 0;
-}
+int main(void) {}
