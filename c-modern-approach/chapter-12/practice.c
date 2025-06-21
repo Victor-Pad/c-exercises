bf07a2ad7a1f5cb4b9cd94663fb702153f5d3f60
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-06-21 03:48:57 -0400

    Try out pointer conditions

--- c-modern-approach/chapter-12/practice.c
+++ c-modern-approach/chapter-12/practice.c
@@ -2,4 +2,13 @@
 
 #define N 10
 
-int main(void) {}
+int main(void) {
+    int a[N], *p = a, *q = &a[N];
+    if (p > q)
+        printf("P bigger than Q\n");
+    else
+        printf("Q bigger than P");
+    ;
+    p += 1;
+    printf("%p", p);
+}
