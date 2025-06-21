72e94758440506a9c1a29bd58d25841b7aaccdba
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-06-21 03:57:49 -0400

    Add code for the 7th project

--- c-modern-approach/chapter-12/projects/project-7.c
+++ c-modern-approach/chapter-12/projects/project-7.c
@@ -1,3 +1,29 @@
 #include <stdio.h>
+#define N 10
+void max_min(int a[], int n, int *max, int *min);
 
-int main(void) {}
+int main(void) {
+    int b[N], i, big, small;
+
+    printf("Enter %d numbers: ", N);
+    for (i = 0; i < N; i++)
+        scanf_s("%d", &b[i]);
+
+    max_min(b, N, &big, &small);
+
+    printf("Largest: %d\n", big);
+    printf("Smallest: %d\n", small);
+
+    return 0;
+}
+void max_min(int a[], int n, int *max, int *min) {
+    int *p;
+    *max = *min = a[0];
+
+    for (p = a; p < a + n - 1; p++) {
+        if (*p > *max)
+            *max = *p;
+        else if (*p < *min)
+            *min = *p;
+    }
+}
