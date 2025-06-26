4ae7897de6b5bb07641870c28b6d9fb3a0c59398
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-06-26 06:06:42 -0400

    Add code for the 12th project

--- c-modern-approach/chapter-13/projects/project-12.c
+++ c-modern-approach/chapter-13/projects/project-12.c
@@ -1,29 +1,37 @@
 #include <stdio.h>
 
+int read_line(char str[30][20 + 1]);
 int main(void) {
-    char terminating_char, sentence[30][20];
-    int index = 1, word_length = 0;
+    char terminating_char, sentence[30][20 + 1];
 
     printf("Enter a sentence: ");
+    int word_amount = read_line(sentence);
 
-    while ((terminating_char = getchar()) != '\n') {
-        if (terminating_char == '!' || terminating_char == '?' ||
-            terminating_char == '.') {
-            break;
+    for (int i = word_amount; i >= 0; i--) {
+        for (int j = 0; j < 20 && sentence[i][j] != '\0'; j++) {
+            printf("%c", sentence[i][j]);
         }
-        sentence[index++] = terminating_char;
+        printf(" ");
+    }
+}
+
+int read_line(char str[30][20 + 1]) {
+    int ch, i, j;
+
+    for (i = 0; i < 30; i++) {
+        while ((ch = getchar()) == ' ')
+            ;
+
+        for (j = 0; j < 20 && ch != ' ' && ch != '\n'; j++) {
+            str[i][j] = ch;
+            ch = getchar();
+        }
+
+        str[i][j] = '\0';
+
+        if (ch == '\n')
+            break;
     }
 
-    for (int i = index - 1; i >= 0; i--) {
-        if (sentence[i] == ' ') {
-            int temp_index = i + 1;
-            for (int j = 0; j < word_length; j++) {
-                putchar(sentence[temp_index + j]);
-            }
-            word_length = 0;
-            putchar(' ');
-        } else
-            word_length++;
-    }
-    putchar(terminating_char);
+    return i;
 }
