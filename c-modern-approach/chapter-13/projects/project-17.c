b96e1de1968c6377ec6b2e6f4551faa0857eb4c1
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-06-27 12:18:45 -0400

    Add code for the 17th project

--- c-modern-approach/chapter-13/projects/project-17.c
+++ c-modern-approach/chapter-13/projects/project-17.c
@@ -1,26 +1,46 @@
 #include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
+
+#define SENTENCE_LEN 100
+
+void read_line(char str[], int n);
+bool is_palindrome(const char *message);
 
 int main(void) {
-    char ch, sentence[100], *ptr_start = sentence, *ptr_end = sentence;
+    char sentence[SENTENCE_LEN + 1];
 
     printf("Enter a message: ");
-    while ((ch = getchar()) != '\n') {
-        if (isalpha(ch))
-            *ptr_end++ = tolower(ch);
-    }
+    read_line(sentence, SENTENCE_LEN + 1);
 
-    ptr_end -= 1;
+    if (is_palindrome(sentence))
+        printf("Palindrome");
+    else
+        printf("Not a palindrome");
 
-    while (ptr_end > sentence) {
-        if (*ptr_start != *ptr_end) {
-            printf("Not a palindrome\n");
-            return 0;
-        }
-        ptr_start++;
-        ptr_end--;
-    }
-
-    printf("Palindrome");
     return 0;
+}
+
+void read_line(char str[], int n) {
+    int ch, i = 0;
+
+    while ((ch = getchar()) != '\n')
+        if (i < n)
+            str[i++] = tolower(ch);
+
+    str[i] = '\0';
+}
+
+bool is_palindrome(const char *message) {
+    const char *start = message, *end = message + strlen(message) - 1;
+
+    while (*start && *end) {
+        if (*start != *end)
+            return false;
+        start++;
+        end++;
+    }
+
+    return true;
 }
