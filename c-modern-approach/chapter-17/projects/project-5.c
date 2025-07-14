f3d5ef6df57103fb27c3f419e4f44d0eb6ab3372
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-07-13 20:09:26 -0400

    Add code for the 5th project

--- c-modern-approach/chapter-17/projects/project-5.c
+++ c-modern-approach/chapter-17/projects/project-5.c
@@ -4,12 +4,13 @@
 
 #define MAX_WORDS 15
 #define WORD_LEN 20
+
 int read_line(char str[], int n);
 
 int main(void) {
     char *words[MAX_WORDS];
     char word[WORD_LEN + 1];
-    int i, num_words = 0;
+    int i, j, num_words = 0;
 
     for (;;) {
         if (num_words >= MAX_WORDS) {
@@ -21,17 +22,37 @@ int main(void) {
         if (read_line(word, WORD_LEN + 1) <= 0)
             break;
 
-        words[i] = malloc(strlen(word) + 1);
+        words[num_words] = malloc(strlen(word) + 1);
         if (words[i] == NULL) {
             printf("No space left");
             break;
         }
 
-        strcpy(words[i], word);
+        strcpy(words[num_words], word);
         num_words++;
     }
 
+    for (i = 0; i < num_words; i++) {
+        int min_index = i;
+
+        for (j = i + 1; j < num_words; j++) {
+            if (strcmp(words[j], words[min_index]) < 0) {
+                min_index = j;
+            }
+        }
+
+        if (min_index != i) {
+            char *temp = words[i];
+            words[i] = words[min_index];
+            words[min_index] = temp;
+        }
+    }
+
     printf("In sorted order: ");
+    for (i = 0; i < num_words; i++)
+        printf("%s ", words[i]);
+
+    return 0;
 }
 
 int read_line(char str[], int n) {
