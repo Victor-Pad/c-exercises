2f4130fa4acebabbcdf8647eb9f2af811457464e
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-07-20 01:41:26 -0400

    Add code for the header and functionality of queue

--- c-modern-approach/chapter-19/projects/project-5/queueADT.c
+++ c-modern-approach/chapter-19/projects/project-5/queueADT.c
@@ -1,3 +1,78 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "queueADT.h"
+
+#define QUEUE_SIZE 100
+
+struct queue_type {
+    int contents[QUEUE_SIZE];
+    int len;
+    int first_empty;
+    int next_removed;
+};
+
+void terminate(char *message) {
+    printf("%s\n", message);
+    exit(EXIT_FAILURE);
+}
+
+Queue create(void) {
+    Queue q = malloc(sizeof(struct queue_type));
+
+    if (q == NULL)
+        terminate("Error in create: malloc failed.");
+
+    q->len = 0;
+    q->first_empty = 0;
+    q->next_removed = 0;
+    return q;
+}
+
+void destroy(Queue q) {
+    free(q);
+}
+
+void wrap_around(Queue q) {
+    if (q->next_removed >= QUEUE_SIZE)
+        q->next_removed = 0;
+    if (q->first_empty >= QUEUE_SIZE)
+        q->first_empty = 0;
+}
+
+bool is_empty(Queue q) {
+    return q->len == 0;
+}
+
+bool is_full(Queue q) {
+    return q->len == QUEUE_SIZE;
+}
+
+void enqueue(Queue q, Item i) {
+    if (is_full(q))
+        terminate("Error in enqueue: queue is full.");
+
+    q->contents[q->first_empty++] = i;
+    wrap_around(q);
+    q->len++;
+}
+
+void dequeue(Queue q) {
+    if (is_empty(q))
+        terminate("Error in dequeue: queue is empty");
+
+    q->next_removed++;
+    wrap_around(q);
+    q->len--;
+}
+
+int peek_front(Queue q) {
+    return q->contents[q->next_removed];
+}
+
+int peek_back(Queue q) {
+    if (q->first_empty == 0)
+        return q->contents[QUEUE_SIZE - 1];
+
+    return q->contents[q->first_empty - 1];
+}
--- c-modern-approach/chapter-19/projects/project-5/queueADT.h
+++ c-modern-approach/chapter-19/projects/project-5/queueADT.h
@@ -4,12 +4,13 @@
 typedef int Item;
 typedef struct queue_type *Queue;
 
-void create(void);
+Queue create(void);
 void destroy(Queue q);
-void enqueue(Queue q);
+void enqueue(Queue q, Item i);
 void dequeue(Queue q);
 int peek_front(Queue q);
 int peek_back(Queue q);
 bool is_empty(Queue q);
+bool is_full(Queue q);
 
 #endif
