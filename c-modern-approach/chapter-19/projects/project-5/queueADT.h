bce9aff3d07b85d23f295552204962e2ef204ca2
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-07-20 00:20:59 -0400

    Update files

--- c-modern-approach/chapter-19/projects/project-5/queueADT.h
+++ c-modern-approach/chapter-19/projects/project-5/queueADT.h
@@ -6,8 +6,8 @@ typedef struct queue_type *Queue;
 
 void create(void);
 void destroy(Queue q);
-void insert(Queue q);
-void remove(Queue q);
+void enqueue(Queue q);
+void dequeue(Queue q);
 int peek_front(Queue q);
 int peek_back(Queue q);
 bool is_empty(Queue q);
