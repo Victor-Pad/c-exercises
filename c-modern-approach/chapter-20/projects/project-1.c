80e9e6b189ff22ca08c3d6c5f32624fa9370c042
Kirash39 <113220462+Kirash39@users.noreply.github.com>
2025-07-22 03:23:20 -0400

    Add code for the last project I'll do from this book!

--- c-modern-approach/chapter-20/projects/project-1.c
+++ c-modern-approach/chapter-20/projects/project-1.c
@@ -1,3 +1,22 @@
 #include <stdio.h>
 
-int main(void) {}
+int main(void) {
+    // unsigned int sign: 1;
+    // unsigned int exponent: 128;
+    // unsigned int fraction: 0;
+    //
+    union {
+        float value;
+        struct {
+            unsigned int fraction : 23;
+            unsigned int exponent : 8;
+            unsigned int sign : 1;
+        } IEEE_float;
+    } u;
+
+    u.IEEE_float.sign = 1;
+    u.IEEE_float.exponent = 128;
+    u.IEEE_float.fraction = 0;
+
+    printf("Float value: %.1f", u.value);
+}
